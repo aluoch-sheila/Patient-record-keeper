b3540098c7cbedbdf49bafd8ddfa74b63a9e0554
aluoch-sheila <aluochsheila1999@gmail.com>
2018-11-28 15:04:11 +0300

    added side bar elements

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template, request, redirect, url_for, abort
+from . import main
+
+from .. import db
+
+
+@main.route('/')
+def index():
+ title = 'Home - Welcome to Tabibu'
+
+ return render_template('index.html', title=title)
