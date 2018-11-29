9f7bc78c4d3dd7c248d1b649e9fcfdecba046da9
aluoch-sheila <aluochsheila1999@gmail.com>
2018-11-29 07:37:02 +0300

    added infomation on how to contact as

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,25 @@ def index():
  title = 'Home - Welcome to Tabibu'
 
  return render_template('index.html', title=title)
+
+
+@main.route('/about')
+def about():
+ title = 'Home - Welcome to Tabibu'
+
+ return render_template('about.html', title=title)
+
+
+@main.route('/team')
+def team():
+ title = 'Home - Welcome to Tabibu'
+
+ return render_template('team.html', title=title)
+
+
+@main.route('/contact')
+def contact():
+ title = 'Home - Welcome to Tabibu'
+
+ return render_template('contact.html', title=title)
+
