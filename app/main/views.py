a43e3806eef202a781012651656952088c9ded9a
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-29 14:10:20 +0300

    used carousel for materialise

--- app/main/views.py
+++ app/main/views.py
@@ -8,9 +8,3 @@ def index():
   title = 'Home - Welcome to Tabibu'
  
   return render_template('index.html', title = title)
-
-@main.route('/about')
-def about():
-  title = 'Home - Patient Record Keeper'
- 
-  return render_template('about.html', title = title)
